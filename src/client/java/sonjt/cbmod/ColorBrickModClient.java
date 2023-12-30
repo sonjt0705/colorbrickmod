cdeb2f1ca9c18eeb1e8752879a945584c7630c1f
root <root@GALAXYBOOKPRO2>
2023-12-30 21:11:18 +0900

    브랜치 분리 작업 앞으로 fabric 버전과 forge 버전으로 분리하여 개발하겠습니다.

---
+++ src/client/java/sonjt/cbmod/ColorBrickModClient.java
@@ -0,0 +1,9 @@
+package sonjt.cbmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ColorBrickModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+	}
+}
--- src/main/java/sonjt/cbmod/ColorBrickMod.java
+++ src/main/java/sonjt/cbmod/ColorBrickMod.java
@@ -2,87 +2,21 @@ package sonjt.cbmod;
 
 import net.minecraft.block.*;
 import net.minecraft.item.*;
-import net.minecraftforge.common.*;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.*;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.*;
 import java.util.stream.Collectors;
 import static sonjt.cbmod.item.Items.*;
 import static sonjt.cbmod.block.Blocks.*;
 
-@Mod(ColorBrickMod.MODID)
-public class ColorBrickMod
-{
-    private static final Logger LOGGER = LogManager.getLogger();
-    public static final String MODID = "colorbrickmod";
+import net.fabricmc.api.ModInitializer;
 
-    public ColorBrickMod() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
-        MinecraftForge.EVENT_BUS.register(this);
-    }
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-    private void setup(final FMLCommonSetupEvent event) {
-        LOGGER.info("Client Initialization");
-        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
-    }
-
-    private void doClientStuff(final FMLClientSetupEvent event) {
-        LOGGER.info("Got Game Settings {}", event.getMinecraftSupplier().get().gameSettings);
-    }
-
-    private void enqueueIMC(final InterModEnqueueEvent event) {
-        InterModComms.sendTo(MODID, "enqueue", () -> {
-            LOGGER.info("MDK Enqueue IMC");
-            return "Hello, world!";
-        });
-    }
-
-    private void processIMC(final InterModProcessEvent event) {
-        LOGGER.info(
-            "Got Inter Mod Communication {}",
-            event.getIMCStream().map(m->m.getMessageSupplier().get()).collect(Collectors.toList())
-        );
-    }
-
-    @SubscribeEvent
-    public void onServerStarting(FMLServerStartingEvent event) {
-        LOGGER.info("Server Initialization");
-    }
-
-    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
-        @SubscribeEvent
-        public static void onItemsRegistry(final RegistryEvent.Register<Item> event) {
-            LOGGER.info("Item Registration");
-            event.getRegistry().registerAll(
-                blueBricksItem,
-                blueBrick,
-                greenBricksItem,
-                greenBrick,
-                redBricksItem,
-                redBrick,
-                yellowBricksItem,
-                yellowBrick
-            );
-        }
-
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> event) {
-            LOGGER.info("Block Registration");
-            event.getRegistry().registerAll(
-                blueBricks,
-                greenBricks,
-                redBricks,
-                yellowBricks
-            );
-        }
-    }
+public class ColorBrickMod implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger();
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Client Initialization");
+		LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+	}
 }
